2abc33f227d8f32c80092912256c6ee722f92c68
Mudream <mudream4869@gmail.com>
2015-01-06 00:42:51 +0800

    complete WindowMsg

--- WindowMsg.cpp
+++ WindowMsg.cpp
@@ -4,30 +4,30 @@
 WindowMsg::WindowMsg(float _left, float _top, float _width, float _height, const char* str)
     :Window(_left, _top, _width, _height){
     printf("Init the msg: [%s]\n", str);
-    isok = false; 
-    ptr = -1;
+    ptr = 0;
     msg = new char[strlen(str) + 1];
     strcpy(msg, str);
-    ptr_x = 0.1, ptr_y = 0.1;
+    return;
+}
+
+WindowMsg::~WindowMsg(){
+    delete[] msg;
     return;
 }
 
 void WindowMsg::TickEvent(int a){
     // TODO: clear dirty thing
-    if(isok) return;
-    ptr++;
     if(msg[ptr] == 0){
-        isok = true;
         return;
     }
-    char c[2] = {msg[ptr], 0};
-    ptr_x += 0.06;
-    if(ptr_x > width)
-        ptr_x = 0.1, ptr_y = ptr_y + 0.1;
-    this->DrawText(ptr_x, ptr_y, c) ;
+    char tmp = msg[ptr+1];
+    msg[ptr+1] = 0;
+    this->DrawText(0.1, 0.1, msg) ;
+    msg[ptr+1] = tmp;
+    ptr++;
     return;
 }
 
 bool WindowMsg::IsOk()const{
-    return isok;
+    return msg[ptr] == 0;
 }
--- WindowMsg.h
+++ WindowMsg.h
@@ -5,13 +5,12 @@
 class WindowMsg : public Window{
 public:
     WindowMsg(float _left, float _top, float _width, float _height, const char* str); 
+    ~WindowMsg();
     void TickEvent(int a);
     bool IsOk() const;
 private:
     int ptr;
-    float ptr_x, ptr_y;
     char* msg;
-    bool isok;
 };
 
 #endif
