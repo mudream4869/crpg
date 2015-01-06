5eae653443ecd4ea72936754db3936e01b1ae11b
Mudream <mudream4869@gmail.com>
2015-01-06 17:39:25 +0800

    ready to init gameobject

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,10 +1,17 @@
+#include <cstdio>
+#include <cstdlib>
 #include "GameObject.h"
 
-std::map<const char*, GameObject*, StrCompare> GameObjectData::gameobject_pool;
+std::map<const char*, GameObjectData::GameObject*, StrCompare> GameObjectData::gameobject_pool;
 std::map<const char*, int, StrCompare> GameObjectData::gameobject_count;
 
 void GameObjectData::InitGameObject(){
-      
+    FILE* finit = fopen("objects/__init__.ini", "r") ;
+    char name[20];
+    while(fscanf(finit, "%s", name) != EOF){
+        
+    }
+    return;
 }
 
 int GameObjectData::GetGameObjectCount(const char* str){
@@ -28,7 +35,7 @@ void GameObjectData::CallGameObject(const char* str){
     return;
 }
 
-void SetGameObjectCount(const char* str, int val){
+void GameObjectData::SetGameObjectCount(const char* str, int val){
     if(gameobject_count.count(str) == 0){
         char* tmp = new char[strlen(str) + 2];
         strcpy(tmp, str);
